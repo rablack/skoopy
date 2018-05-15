5ede659f8dd85c0e045a8f7884088b4a0216bb79
Robert Black <rablack@users.noreply.github.com>
2018-05-14 18:39:25 -0700

    Resolves #9. Added use of registry to scanner

--- skoopy/scanner.py
+++ skoopy/scanner.py
@@ -1,13 +1,25 @@
 #!/usr/env python3
 
 from skoopy.transport import TransportBluepy
+from skoopy.registry import SkoobotRegistry
 
 def scan():
     transport = TransportBluepy()
+    registry = SkoobotRegistry()
 
     rawDevices = transport.findRawDevices()
     for device in rawDevices:
-        print(transport.rawDeviceInfoStr(device))
+        # print(transport.rawDeviceInfoStr(device))
+        addr = device.addr
+        registry.addSkoobot(addr)
+        name = registry.getSkoobotsByAddress(addr)[0][1]
+        if registry.getDefaultName() == None:
+            registry.setDefault(name)
+        defaultText = " (default)" if registry.getDefaultName() == name else ""
+        msg = "Added Skoobot {0:s} to registry with name {1:s}{2:s}"
+        print(msg.format(addr, name, defaultText))
+    print("Saving to list of Skoobots to registry {0:s}".format(registry.registryPath))
+    registry.save()
 
 if __name__ == "__main__":
     scan()
