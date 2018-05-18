92827cc192e315625464616eeb492adc04397bdd
Robert Black <rablack@users.noreply.github.com>
2018-05-17 21:33:50 -0700

    Resolves #15: SkoobotController class added with basic movement commands

--- skoopy/controller.py
+++ skoopy/controller.py
@@ -1,72 +1,152 @@
-#!/usr/env python3
+#!/usr/env python
 
 from skoopy.transport import TransportBluepy
 from skoopy.registry import SkoobotRegistry
 from pathlib import Path
+from bluepy.btle import BTLEException
 import time
 
-skoobot_file_path = Path(Path.home(), ".skoobots")
+CMD_RIGHT = 0x10
+CMD_LEFT = 0x11
+CMD_FORWARD = 0x12
+CMD_BACKWARD = 0x13
+CMD_STOP = 0x14
+CMD_SLEEP = 0x15
 
-transport = TransportBluepy()
+#    cmd_step_mode = 0x16
+#    cmd_buzzer = 0x17
+#    cmd_get_ambient = 0x21
+#    cmd_get_distance = 0x22
+#    cmd_record = 0x30
+#    cmd_increase_gain = 0x31
+#    cmd_decrease_gain = 0x32
+#    cmd_rover_mode = 0x40
 
-cmdUUID = "00001525-1212-efde-1523-785feabcd123"
-dataUUID = "00001524-1212-efde-1523-785feabcd123"
+class SkoobotController:
+    """
+    Control API for Skoobots
+    """
 
+    def __init__(self):
+        self.transport = TransportBluepy()
+        self.registry = SkoobotRegistry()
+        self.uuids = {
+            "cmd" : "00001525-1212-efde-1523-785feabcd123",
+            "data" : "00001524-1212-efde-1523-785feabcd123"
+        }
+        self.connectedSkoobot = None
 
-registry = SkoobotRegistry()
-print("Loaded {1:d} skoobots from {0:s}".format(registry.registryPath, len(registry.registry)))
-defaultName = registry.getDefaultName()
-if defaultName == None:
-    print("No default Skoobot")
-    exit(1)
-skoobot = registry.getSkoobotsByName(defaultName)[0]
-bleAddr = skoobot[0]
+    def connect(self, name=None, addr=None):
+        """
+        Connect to the given Skoobot.
+        If no Skoobot is given, connects to the default.
+        Returns the address of the connected Skoobot if successful;
+        None otherwise.
+        """
+        addrList = []
 
-#class SkooController:
-#    def __init__(self):
+        if addr != None:
+            if isinstance(addr, str):
+                addrList.append(addr)
+            else:
+                raise TypeError("addr should be a string")
+        else:
+            if name == None:
+                name = self.registry.getDefaultName()
+            elif not isinstance(name, str):
+                raise TypeError("name should be a string")
+            if name != None:
+                skoobots = self.registry.getSkoobotsByName(name)
+                addrList = [ bot[0] for bot in skoobots ]
+            else:
+                raise RuntimeError("No default skoobot defined")
         
+            if len(addrList) == 0:
+                raise ValueError("No Skoobot with name {0:s} found".format(name))
 
-cmd_right = 0x10
-cmd_left = 0x11
-cmd_forward = 0x12
-cmd_backward = 0x13
-cmd_stop = 0x14
-cmd_sleep = 0x15
-cmd_step_mode = 0x16
-cmd_buzzer = 0x17
-cmd_get_ambient = 0x21
-cmd_get_distance = 0x22
-cmd_record = 0x30
-cmd_increase_gain = 0x31
-cmd_decrease_gain = 0x32
-cmd_rover_mode = 0x40
+        if self.connectedSkoobot != None:
+            self.disconnect()
 
-print("Attempting to contact Skoobot at '{0:s}'".format(bleAddr))
+        for botAddr in addrList:
+            try:
+                self.transport.connect(botAddr)
+                self.connectedSkoobot = botAddr
+                break
+            except BTLEException:
+                pass
 
-transport.connect(bleAddr)
-cmdList = transport.getRawCharacteristicsByUUID(cmdUUID)
-if len(cmdList) == 0:
-    print("No command characteristic found\n")
-    exit(1)
+        return self.connectedSkoobot
 
-cmd = cmdList[0]
+    def disconnect(self):
+        self.transport.disconnect()
+        self.connectedSkoobot = None
 
-cmdData = bytearray(1)
+    def sendCommand(self, data, waitForResponse=False):
+        if self.connectedSkoobot == None:
+            raise RuntimeError("BLE not connected")
+        data = int(data);
+        cmdBytes = data.to_bytes(1, byteorder="little") 
+        characteristics = self.transport.getRawCharacteristicsByUUID(self.uuids["cmd"])
+        if len(characteristics) == 0:
+            raise RuntimeError("cmd characteristic not supported by firmware")
+        cmd = characteristics[0]
+        cmd.write(cmdBytes, waitForResponse)
 
-print("Right")
-cmdData[0] = cmd_right
-cmd.write(cmdData, False)
-time.sleep(1)
+    def cmdRight(self):
+       self.sendCommand(CMD_RIGHT, True)
 
-print("Left")
-cmdData[0] = cmd_left
-cmd.write(cmdData, False)
-time.sleep(1)
+    def cmdLeft(self):
+       self.sendCommand(CMD_LEFT, True)
 
-print("Sleep")
-cmdData[0] = cmd_sleep
-cmd.write(cmdData, False)
-time.sleep(1)
+    def cmdForward(self):
+       self.sendCommand(CMD_FORWARD, True)
 
-transport.disconnect()
-print("Done")
+    def cmdBackward(self):
+       self.sendCommand(CMD_BACKWARD, True)
+
+    def cmdStop(self):
+       self.sendCommand(CMD_STOP, True)
+
+    def cmdSleep(self):
+       self.sendCommand(CMD_SLEEP, True)
+
+def control():
+    controller = SkoobotController()
+
+    print("Attempting to contact Skoobot...")
+    
+    addr = controller.connect()
+    if addr == None:
+        print("Unable to connect to skoobot")
+        exit(1)
+    
+    print("Right")
+    controller.cmdRight()
+    time.sleep(1)
+    
+    print("Left")
+    controller.cmdLeft()
+    time.sleep(1)
+    
+    print("Forward")
+    controller.cmdForward()
+    time.sleep(1)
+    
+    print("Backward")
+    controller.cmdBackward()
+    time.sleep(0.5)
+    
+    print("Stop")
+    controller.cmdStop()
+    time.sleep(1)
+    
+    print("Sleep")
+    controller.cmdSleep()
+    time.sleep(1)
+    
+    controller.disconnect()
+    print("Done")
+
+
+if __name__ == "__main__":
+    control()
